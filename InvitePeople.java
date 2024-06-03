49ce15da200325043a44e9013ebc99b44c53df46
Pritam Ghosh <rintughosh1aa@gmail.com>
2024-06-03 19:55:04 +0530

    Recursion Day 10

---
+++ InvitePeople.java
@@ -0,0 +1,16 @@
+//Find the number of ways in which you can invite n people to your party, single or in pairs whwere n=4
+public class InvitePeople {
+    public static int callGuests(int n) {
+        if (n <= 1) {
+            return 1;
+        }
+        int ways1 = callGuests(n - 1);
+        int ways2 = (n - 1) * callGuests(n - 2);
+        return ways1 + ways2;
+    }
+
+    public static void main(String[] args) {
+        int n = 4;
+        System.out.println(callGuests(n));
+    }
+}
---
+++ Subset.java
@@ -0,0 +1,30 @@
+//Print all the subsets of a set of first n natural numbers. n=3
+
+import java.util.ArrayList;
+
+public class Subset {
+    public static void printSubset(ArrayList<Integer> subset) {
+        for (int i = 0; i < subset.size(); i++) {
+            System.out.print(subset.get(i) + " ");
+        }
+        System.out.println();
+    }
+
+    public static void findSubsets(int n, ArrayList<Integer> subset) {
+        if (n == 0) {
+            printSubset(subset);
+            return;
+        }
+        subset.add(n);
+        findSubsets(n - 1, subset);
+
+        subset.remove(subset.size() - 1);
+        findSubsets(n - 1, subset);
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        ArrayList<Integer> subset = new ArrayList<Integer>();
+        findSubsets(n, subset);
+    }
+}
